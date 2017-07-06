ab4e98eb5badef4f275d9e46b7a6dfca1c1383ac
PiGuy2 <rsamm3@gmail.com>
2017-07-05 21:53:21 -0400

    Added shuffling

--- Assets/Scripts/GameRunner.cs
+++ Assets/Scripts/GameRunner.cs
@@ -4,13 +4,66 @@ using UnityEngine;
 
 public class GameRunner : MonoBehaviour {
 
+	Card[] deck = new Card[50];
+	int drawNum = 0;
+
 	// Use this for initialization
 	void Start () {
-		
+		shuffleDeck();
+		// -----
+		int[] count = new int[6];
+		for (int i = 0; i < 100; i++) {
+			count[(int)draw()]++;
+			// print(deck[i]);
+		}
+		for (int i = 0; i < 6; i++) {
+			print("Count 2 " + i.ToString() + ": " + count[i].ToString());
+		}
+		draw();
+		// -----
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+	void shuffleDeck () {
+		int[] sel = new int[5];
+		for (int i = 0; i < 50;) {
+			int n = Random.Range(1, 6);
+			if (sel[n - 1] < 10) {
+				deck[i] = (Card)n;
+				sel[n - 1] += 1;
+				i++;
+			}
+		}
+		int[] count = new int[6];
+		for (int i = 0; i < 50; i++) {
+			count[(int)deck[i]]++;
+			// print(deck[i]);
+		}
+		for (int i = 0; i < 6; i++) {
+			print("Count " + i.ToString() + ": " + count[i].ToString());
+		}
+		drawNum = 0;
+	}
+
+	Card draw () {
+		if (drawNum >= 50) {
+			shuffleDeck();
+			// print("shuffle");
+		}
+		drawNum++;
+		return deck[drawNum - 1];
+	}
+
+	enum Card : int {
+		food = 1,
+		wood = 2,
+		metal = 3,
+		money = 4,
+		fuel = 5}
+
+	;
 }
