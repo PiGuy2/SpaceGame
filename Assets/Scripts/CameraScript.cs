6dfb797907de712c67f5ec28c857caf83fc64c89
PiGuy2 <rsamm3@gmail.com>
2017-07-07 17:08:22 -0400

    work on multiplayer

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -1,16 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class CameraScript : MonoBehaviour {
+public class CameraScript : NetworkBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		transform.position = new Vector3(0, 3, 0);
+		transform.eulerAngles = new Vector3(90, 0, 0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (!isLocalPlayer) {
+			return;
+		}
+
+		var x = Input.GetAxis("Horizontal") * 0.1f;
+		var z = Input.GetAxis("Vertical") * 0.1f;
+
+		transform.Translate(x, 0, z, Space.World);
 	}
 }
